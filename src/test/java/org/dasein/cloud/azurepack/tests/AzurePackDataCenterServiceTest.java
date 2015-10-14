8beedacfd722d4e4f8263ff56025c23901d5ae7f
Jeffrey Yan <jeffrey.yan@software.dell.com>
2015-10-14 17:46:52 +0800

    Unit test for DataCenterServiceTest.listDataCenters

---
+++ src/test/java/org/dasein/cloud/azurepack/tests/AzurePackDataCenterServiceTest.java
@@ -0,0 +1,117 @@
+/*
+ *  *
+ *  Copyright (C) 2009-2015 Dell, Inc.
+ *  See annotations for authorship information
+ *
+ *  ====================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ====================================================================
+ *
+ */
+
+package org.dasein.cloud.azurepack.tests;
+
+import mockit.Mock;
+import mockit.MockUp;
+import org.apache.commons.collections.IteratorUtils;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.dasein.cloud.CloudException;
+import org.dasein.cloud.CloudProvider;
+import org.dasein.cloud.InternalException;
+import org.dasein.cloud.azurepack.AzurePackDataCenterService;
+import org.dasein.cloud.azurepack.AzurePackTestsBase;
+import org.dasein.cloud.azurepack.model.WAPCloudModel;
+import org.dasein.cloud.azurepack.model.WAPCloudsModel;
+import org.dasein.cloud.dc.DataCenter;
+import org.dasein.cloud.util.requester.DaseinRequestExecutor;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.dasein.cloud.azurepack.HttpMethodAsserts.assertGet;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Jeffrey Yan on 10/14/2015.
+ *
+ * @author Jeffrey Yan
+ * @since 2015.09.1
+ */
+public class AzurePackDataCenterServiceTest extends AzurePackTestsBase {
+
+  private final String CLOUD_RESOURCES = "%s/%s/services/systemcenter/vmm/Clouds";
+  private final String DATA_CENTER_ID = UUID.randomUUID().toString();
+  private final String DATA_CENTER_NAME = "TEST_DATA_CENTER";
+
+
+  private AzurePackDataCenterService azurePackDataCenterService;
+
+  @Before
+  public void setUp() throws CloudException, InternalException {
+    super.setUp();
+    azurePackDataCenterService = new AzurePackDataCenterService(azurePackCloudMock);
+  }
+
+  @Test
+  public void listDataCentersShouldReturnCorrectResult() throws CloudException, InternalException, LoginException {
+    new MockUp<DaseinRequestExecutor>() {
+      @Mock
+      public void $init(CloudProvider provider, HttpClientBuilder clientBuilder, HttpUriRequest request, ResponseHandler handler) {
+        assertGet(request, String.format(CLOUD_RESOURCES, ENDPOINT, ACCOUNT_NO));
+      }
+
+      @Mock
+      public Object execute() {
+        WAPCloudsModel wapCloudsModel = new WAPCloudsModel();
+        wapCloudsModel.setOdataMetadata("https://smapi-server:30006/2222aa22-22a2-2a22-2a22-2aaaaa2aaaaa/services/systemcenter/vmm/$metadata#Clouds");
+
+        List<WAPCloudModel> wapCloudModels = new ArrayList<WAPCloudModel>();
+        WAPCloudModel invalidWapCloudModel = new WAPCloudModel();
+        invalidWapCloudModel.setId("3333b333-b3bb-333b-333b-3bbb33333bbb");
+        invalidWapCloudModel.setName("KatalCloud");
+        invalidWapCloudModel.setStampId("444c4444-c44c-4444-c4c4-44ccc4444c4c");
+        wapCloudModels.add(invalidWapCloudModel);
+
+        WAPCloudModel validWapCloudModel = new WAPCloudModel();
+        validWapCloudModel.setId(REGION);
+        validWapCloudModel.setName(DATA_CENTER_NAME);
+        validWapCloudModel.setStampId(DATA_CENTER_ID);
+        wapCloudModels.add(validWapCloudModel);
+
+        wapCloudsModel.setClouds(wapCloudModels);
+        return wapCloudsModel;
+      }
+    };
+
+    List<DataCenter> dataCenters = IteratorUtils.toList(
+            azurePackDataCenterService.listDataCenters(REGION).iterator());
+
+    assertEquals("listDataCenters doesn't return correct data center size", 1, dataCenters.size());
+    DataCenter dataCenter = dataCenters.get(0);
+    assertEquals("listDataCenters doesn't return correct data center", REGION, dataCenter.getRegionId());
+    assertEquals("listDataCenters doesn't return correct data center", DATA_CENTER_ID, dataCenter.getProviderDataCenterId());
+    assertEquals("listDataCenters doesn't return correct data center", DATA_CENTER_NAME, dataCenter.getName());
+    assertTrue("listDataCenters doesn't return correct data center", dataCenter.isActive());
+    assertTrue("listDataCenters doesn't return correct data center", dataCenter.isAvailable());
+  }
+}
+
+
+
---
+++ src/test/java/org/dasein/cloud/azurepack/tests/HttpMethodAsserts.java
@@ -0,0 +1,120 @@
+/*
+ *  *
+ *  Copyright (C) 2009-2015 Dell, Inc.
+ *  See annotations for authorship information
+ *
+ *  ====================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ====================================================================
+ *
+ */
+
+package org.dasein.cloud.azurepack;
+
+import junit.framework.Assert;
+import org.apache.commons.collections.Closure;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.http.Header;
+import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.dasein.cloud.util.requester.streamprocessors.XmlStreamToObjectProcessor;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
+
+/**
+ * Created by vmunthiu on 9/9/2015.
+ */
+public class HttpMethodAsserts {
+  public static void assertGet(HttpUriRequest actualHttpRequest, String expectedUrl) {
+    assertHttpMethod(actualHttpRequest, "GET", expectedUrl);
+  }
+
+  public static void assertGet(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders) {
+    assertHttpMethod(actualHttpRequest, "GET", expectedUrl, expectedHeaders);
+  }
+
+  public static void assertDelete(HttpUriRequest actualHttpRequest, String expectedUrl) {
+    assertHttpMethod(actualHttpRequest, "DELETE", expectedUrl);
+  }
+
+  public static void assertDelete(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders) {
+    assertHttpMethod(actualHttpRequest, "DELETE", expectedUrl, expectedHeaders);
+  }
+
+  public static void assertPut(HttpUriRequest actualHttpRequest, String expectedUrl) {
+    assertHttpMethod(actualHttpRequest, "PUT", expectedUrl);
+  }
+
+  public static void assertPut(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders) {
+    assertHttpMethod(actualHttpRequest, "PUT", expectedUrl, expectedHeaders);
+  }
+
+  public static void assertPut(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders, Object expectedEntity) {
+    assertHttpMethod(actualHttpRequest, "PUT", expectedUrl, expectedHeaders, expectedEntity);
+  }
+
+  public static void assertPost(HttpUriRequest actualHttpRequest, String expectedUrl) {
+    assertHttpMethod(actualHttpRequest, "POST", expectedUrl);
+  }
+
+  public static void assertPost(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders) {
+    assertHttpMethod(actualHttpRequest, "POST", expectedUrl, expectedHeaders);
+  }
+
+  public static void assertPost(HttpUriRequest actualHttpRequest, String expectedUrl, Header[] expectedHeaders, Object expectedEntity) {
+    assertHttpMethod(actualHttpRequest, "POST", expectedUrl, expectedHeaders, expectedEntity);
+  }
+
+  private static void assertHttpMethod(HttpUriRequest actualHttpRequest, String expectedHttpMethod, String expectedUrl) {
+    assertEquals(String.format("Method does not performs a HTTP %s", expectedHttpMethod), expectedHttpMethod, actualHttpRequest.getMethod());
+    assertEquals(String.format("Method does not performs a HTTP %s at the correct URL", expectedHttpMethod), expectedUrl, actualHttpRequest.getURI().toString());
+  }
+
+  private static void assertHttpMethod(final HttpUriRequest actualHttpRequest, String expectedHttpMethod, String expectedUrl, Header[] expectedHeaders)
+  {
+    assertHttpMethod(actualHttpRequest, expectedHttpMethod, expectedUrl);
+    assertNotNull(actualHttpRequest.getAllHeaders());
+    CollectionUtils.forAllDo(Arrays.asList(expectedHeaders), new Closure() {
+      @Override
+      public void execute(Object input) {
+        final Header expectedHeader = (Header) input;
+        boolean headerExists = CollectionUtils.exists(Arrays.asList(actualHttpRequest.getAllHeaders()), new Predicate() {
+          @Override
+          public boolean evaluate(Object object) {
+            Header actualHeader = (Header) object;
+            return expectedHeader.getName().equals(actualHeader.getName()) && expectedHeader.getValue().equals(actualHeader.getValue());
+          }
+        });
+        assertTrue(String.format("Expected %s header not found in the request or found with the wrong value in the request", expectedHeader.getName()), headerExists);
+      }
+    });
+  }
+
+  private static void assertHttpMethod(HttpUriRequest actualHttpRequest, String expectedHttpMethod, String expectedUrl, Header[] expectedHeaders, Object expectedEntity) {
+    assertHttpMethod(actualHttpRequest, expectedHttpMethod, expectedUrl, expectedHeaders);
+    assertTrue("Incorrect httpRequest found in the call", actualHttpRequest instanceof HttpEntityEnclosingRequestBase);
+
+    try {
+      Object actualEntity = new XmlStreamToObjectProcessor().read(((HttpEntityEnclosingRequestBase)actualHttpRequest).getEntity().getContent(), expectedEntity.getClass());
+      assertReflectionEquals("Incorrect value(s) found in the XML body of the request", expectedEntity, actualEntity);
+    } catch (Exception e) {
+      Assert.fail("Incorrect XML body found in the request");
+    }
+  }
+}
