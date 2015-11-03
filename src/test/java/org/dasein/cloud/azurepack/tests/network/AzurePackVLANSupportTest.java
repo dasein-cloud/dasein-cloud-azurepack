4062ce3ffaeb3bfb668a4530898d438187ed6789
JanewzhWang <Jane.Wang@software.dell.com>
2015-11-03 14:31:43 +0800

    Merge fix

--- src/test/java/org/dasein/cloud/azurepack/tests/network/AzurePackVLANSupportTest.java
+++ src/test/java/org/dasein/cloud/azurepack/tests/network/AzurePackVLANSupportTest.java
@@ -1,3 +1,24 @@
+/*
+ *  *
+ *  Copyright (C) 2009-2015 Dell, Inc.
+ *  See annotations for authorship information
+ *
+ *  ====================================================================
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  ====================================================================
+ *
+ */
+
 package org.dasein.cloud.azurepack.tests.network;
 
 import static org.dasein.cloud.azurepack.tests.HttpMethodAsserts.*;
@@ -43,6 +64,12 @@ import org.dasein.cloud.util.requester.entities.DaseinObjectToJsonEntity;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Created by Jane Wang on 10/20/2015.
+ *
+ * @author Jane Wang
+ * @since 2015.09.1
+ */
 public class AzurePackVLANSupportTest extends AzurePackTestsBaseWithLocation {
 
 	private final String VM_NETWORKS = "%s/%s/services/systemcenter/vmm/VMNetworks";
